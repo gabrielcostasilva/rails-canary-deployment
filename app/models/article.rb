ac1c6b42018a45b9c64690a5c08cfcb183401007
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2024-10-04 18:03:52 -0300

    Created retrocompatibility

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,23 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
     
+    before_save :replace_title_with_subject
+    before_update :replace_title_with_subject
+    after_find :replace_title_with_subject
+
+    private 
+        def replace_title_with_subject
+            begin
+                self.subject = self.title
+
+            rescue => e
+                puts e.message
+            end
+            
+            begin
+                self.title = self.subject
+            rescue => e
+                puts e.message
+            end
+        end
 end
